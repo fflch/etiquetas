00e090381d167c624925b18de04c77dbe7486c5a
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-10-28 16:38:34 -0300

    update

--- app/Http/Controllers/EtiquetaController.php
+++ app/Http/Controllers/EtiquetaController.php
@@ -33,4 +33,27 @@ class EtiquetaController extends Controller
         $pimaco->output();
 
     }
+
+    public function fixed(){
+
+        $pimaco = new Pimaco('A4356'); # 
+
+        $conteudo = '<br><b>FFLCH-USP</b><br>
+                     Rua do Lago, 717 - Diretoria <br>
+                     Cidade Universitária <br> 
+                     05508-080 São Paulo - SP';
+
+        foreach(range(1, 33) as $i){
+            $tag = new Tag();            
+            $tag->p(
+                view('pdfs.etiquetas',[
+                    'conteudo' => $conteudo
+                ])
+            );
+            $pimaco->addTag($tag);
+        }
+
+        $pimaco->output();
+
+    }
 } 
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\EtiquetaController;
 
 
 Route::get('/etiquetas',[EtiquetaController::class,'etiquetas']);
+Route::get('/fixed',[EtiquetaController::class,'fixed']);
